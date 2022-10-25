cf4b8b8aca60775221b537bc6ad94a22892f93c5
Tseso <tserensodnom.enkh@gmail.com>
2022-10-25 23:37:43 +0900

    bug fix

--- api/utils.js
+++ api/utils.js
@@ -4,6 +4,7 @@ const getMnDef = async (word) => {
     console.log('word :>> ', word);
     const mnData = await getMonDefByEng(word);
     console.log('mnData :>> ', mnData);
+    if (Object.keys(mnData).length === 0) return "Oldsongui"
     const mnDef = mnData["Item"]["mn"]
     return mnDef ? mnDef : ""
 };
